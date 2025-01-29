2d9f205bd816738912ca7f56d990c59906a489d9
221B288 <221B288@juet.co.in>
2025-01-29 15:13:35 +0530

    221B288 LE-2 AP lab-3

---
+++ LE-2/Q2/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        Voice2 voice2 = new Voice2();
+        voice2.templateMethod();
+    }
+}
---
+++ LE-2/Q2/Voice2.java
@@ -0,0 +1,8 @@
+class Voice2 {
+    private Voice voice = new Voice();
+
+    void templateMethod() {
+        voice.prepareVoice();
+        voice.hear();
+    }
+}
